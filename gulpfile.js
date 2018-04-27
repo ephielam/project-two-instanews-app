f235619c83d5dde499e13433925b93e20d2ae938
Ephie <ephielam@gmail.com>
2018-04-27 12:42:57 +0100

    Project day first update

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,16 @@ var browserSync = require('browser-sync').create();
 var eslint = require('gulp-eslint');
 var sass = require("gulp-sass"),
  autoprefixer = require("gulp-autoprefixer"),
- cssnano = require("gulp-cssnano"),
- rename = require("gulp-rename");
+ cssnano = require("gulp-cssnano");
+var concat = require("gulp-concat");
+//image optimisation and resizing npms
+// var image = require("gulp-image");
+// var imageresize = require("gulp-image-resize");
 
 
 gulp.task('scripts', function(done) {
    return gulp
-       .src(['./js/*.js', '!node_modules/**'])
+       .src(['./js/*.js' , '!node_modules/**'])
        .pipe(eslint())
        .pipe(eslint.format())
        .pipe(eslint.failAfterError())
@@ -35,10 +38,23 @@ gulp.task('sass', function(done) {
      })
    )
    .pipe(cssnano())
-   .pipe(rename('style.min.css'))
+   .pipe(concat('style.min.css'))
    .pipe(gulp.dest('./build/css'))
   .pipe(browserSync.stream());
 });
+//images optimisation for future projects
+// gulp.task('image', function (done) {
+//   return gulp
+//   .src('./assets/img/*')
+//   .pipe(imageresize({
+//       width : 640,
+//       height : 640,
+//       crop : true,
+//       upscale : false
+//     }))
+//     .pipe(image())
+//     .pipe(gulp.dest('./build/img'));
+// });
 
 gulp.task('serve', function() {
    browserSync.init({
--- js/script.js
+++ js/script.js
@@ -1,12 +1,66 @@
-var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
-url += '?' + $.param({
-  'api-key': "8ac58e86a8aa4a118f15cdf99ab41333"
-});
-$.ajax({
-  url: url,
-  method: 'GET',
-}).done(function(result) {
-  console.log(result);
-}).fail(function(err) {
-  throw err;
+// var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
+// url += '?' + $.param({
+//   'api-key': "8ac58e86a8aa4a118f15cdf99ab41333",
+//   'callback': "callback"
+// });
+// $.ajax({
+//   url: url,
+//   method: 'GET',
+// }).done(function(result) {
+//   console.log(result);
+// }).fail(function(err) {
+//   throw err;
+// });
+//
+// $( "select" ).change(function() {   // <<<<< this is the correct syntax
+//   var str = "";
+//   $( "select option:selected" ).each(function() {
+//     str += $( this ).text() + " ";
+//   });
+// })
+//
+// $(function() {
+//   //set variables for data and HTML targets
+//   var newImage newsHeadline = $('.dropdown');
+//
+//   $('select').on('change', function(event) {
+//     event.preventDefault();
+//
+//     //Empty vars each time the form is submitted
+//     $albumList.empty();
+//     albumData, albumItems = '';
+//
+//     //get the search string
+//     artistName = $('#artist-name').val(),itunesUrl = "https://itunes.apple.com/search?entity=album&limit=6&term=" + artistName;
+//
+//     //Get data from the api
+//     $.ajax({
+//       method: 'GET',
+//       url: itunesUrl,
+//       dataType: 'jsonp'
+//     })
+//     //If it works...
+//     .done(function(data) {
+//       albumData = data.results;
+//       //For each loop using keys and values for list items
+//       $.each(albumData, function(key, value) {
+//         albumItems += '<li>';
+//         albumItems += '<img src="' + value.artworkUrl100 + '" />';
+//         albumItems += '<p>'+
+//           value.collectionName + '</p></li>';
+//       });
+//       //Output the entire list to the <ul>
+//       $albumList.append(albumItems);
+//     })
+//     //If it fails, output an error
+//     .fail(function() {
+//       $albumList.append('<li>Sorry! There was a problem, please try search again.</li>');
+//     });
+//   });
+// });
+
+
+
+$( ".dropdown" ).change(function() {
+  alert( "Handler for .change() called." );
 });
