de51cc2b47eae757a262a8ef5a5924806a8dbb02
Ephie <ephielam@gmail.com>
2018-05-01 15:40:07 +0100

    jquery solved

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var browserSync = require('browser-sync').create();
-var eslint = require('gulp-eslint');
+// var eslint = require('gulp-eslint');
 var sass = require("gulp-sass");
 var autoprefixer = require("gulp-autoprefixer");
 var cssnano = require("gulp-cssnano");
@@ -15,9 +15,9 @@ var concat = require("gulp-concat");
 gulp.task('scripts', function(done) {
    return gulp
        .src(['./js/*.js' , '!node_modules/**'])
-       .pipe(eslint())
-       .pipe(eslint.format())
-       .pipe(eslint.failAfterError())
+       // .pipe(eslint())
+       // .pipe(eslint.format())
+       // .pipe(eslint.failAfterError())
        .pipe(uglify())
        .pipe(rename({ extname: '.min.js' }))
        .pipe(gulp.dest('./build/js'));
--- js/script.js
+++ js/script.js
@@ -1,63 +1,105 @@
-(function($) {
-  var nytData, 
-      endpoint,
-      $loader = $('.ajax-loader'),
-      $stories = $('.stories');
-  
-  $('.dropdown').on('change',
-                    function () {
-    var section = $(this).val();
+// (function($) {
+//   //declare variables
+//   var nytData, 
+//       endpoint,
+//       $loader = $('.ajax-loader'),
+//       $stories = $('.stories');
+
+//   //upon selction of dropdown menu item
+//   $('.dropdown').on('change',
+//                     function () {
+//     var section = $(this).val();
     
-    endpoint = 'https://api.nytimes.com/svc/topstories/v2/' + section + '.json?api-key=8ac58e86a8aa4a118f15cdf99ab41333';
-    $stories.empty();
-    nytData = '';
-    nytItems = '';
+//     //empty the stories div first
+//     endpoint = 'https://api.nytimes.com/svc/topstories/v2/' + section + '.json?api-key=8ac58e86a8aa4a118f15cdf99ab41333';
+//     $stories.empty();
+//     nytData = '';
+//     nytItems = '';
     
     
-    //css changes here
-    //hide loader here
-    
-    $.ajax({
-      method: 'GET',
-      url: endpoint,
-      dataType: 'json'
-    })
-    .done(function(data) {
-      nytData = data.results;
-      var articleLink, articleTitle, articleImageUrl;
+//     //css changes here
+//     $loader.css("display","block"),
+
+//     $.ajax({
+//       method: 'GET',
+//       url: endpoint,
+//       dataType: 'json',
+//     })
+//     .done(function(data) {
+//       nytData = data.results;
+//       var articleLink, articleTitle, articleImageUrl;
       
-      if(nytData.lenth !==0){
-        nytItems += '<ul';
-        $.each(function(key, value) {
-          articleImageUrl = value.multimedia[4].url;
-          articleTitle = value.title;
-          articleLink = value.url;
-         nytItems += '<li class="article-item">';
-         nytItems += '<a href=" ' +articleLink +'" target="_blank">';
-          nytItems += '<div class="inner-item-wrapper">';
-          nytItems += '<div class="article" style="background-image:url('+ articleImageUrl + ')">';
-           nytItems += '<div class="story-meta">';
-           nytItems += '<p>' +
-             (articleTitle || "This story has no description.") + '</p>'
-            nytItems += '</div>';
-            nytItems += '</div>';
-            nytItems += '</div>';
-            nytItems += '</a>';
-           nytItems += '</li>';
-        });
-           nytItems += '</ul';
+//       //if data is returned
+//       if(nytData.length !==0){
+//         //start unordered list
+//         nytItems += '<ul'>,
+//         //for each story
+//         $.each(function(key, value) {
+//           articleImageUrl = value.multimedia[4].url,
+//           articleTitle = value.["abstract"],
+//           articleLink = value.url,
+//           nytItems += '<li class="article-item">',
+//           nytItems += '<a href=" ' +articleLink +'" target="_blank">',
+//           nytItems += '<div class="inner-item-wrapper">',
+//           nytItems += '<div class="article" style="background-image:url('+ articleImageUrl + ')">',
+//           nytItems += '<div class="story-meta">',
+//           nytItems += '<p>' +
+//             (articleTitle || "This story has no description.") + '</p>',
+//           nytItems += '</div>',
+//           nytItems += '</div>',
+//           nytItems += '</div>',
+//           nytItems += '</a>',
+//           nytItems += '</li>',
+//         }),
+//           nytItems += '</ul>';
            
-      } else {
-         nytItems += '<p class="feedback">Sorry!</p>';
-      }
+//       } else {
+//        nytItems += '<p class="feedback">Sorry!</p>';
+//        }
       
-      $stories.hide().fadeIn('fast').append(nytItems);
+//         $stories.hide().fadeIn('fast').append(nytItems);
+//       })
+//     .fail(function(){
+//       $stories.append('<p class="feedback">Sorry2!</p>');
+//     })
+//     .always(function() {
+//       $loader.hide();
+//     });
+//   });
+// })(jQuery);
+
+
+$(function(){
+  var list ;
+  $('.dropdown').on('change', function(event) {
+    event.preventDefault();
+    var section = $(this).val();
+    $('.dump').empty();
+    list = '',
+    $('.ajax-loader').css("display","block");
+    $.ajax({
+     method: 'GET',
+     url: 'https://api.nytimes.com/svc/topstories/v2/' + section +'.json?api-key=8ac58e86a8aa4a118f15cdf99ab41333'
+     })
+    .done(function(data) {
+       if(data.results.length !==0){
+        $.each(data.results, function(key, value){
+         if(key < 5){
+           list += '<li><a href="'+value.url+'" target="_blank"><div class="inner" style="background-image:url('+value.multimedia[4].url+ ')">'+'<p>' +value.abstract + '<p></div></a></li>';
+          }
+        });
+        $('.dump').append(list);
+      }
     })
-    .fail(function(){
-      $stories.append('<p class="feedback">Sorry2!</p>');
+
+          
+    .fail(function() {
+      $('.dump').append('Sorry there was an error.');
     })
-    .always(function() {
-      $loader.hide();
-    });
+    .always(function(){
+      $('.ajax-loader').hide();
+      $('li').slice(0,4);
+    });  
   });
-})(jQuery);
+});
+
